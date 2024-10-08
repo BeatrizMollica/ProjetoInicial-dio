c19829e601f9768e013aee9875289634fe3e5e23
BeatrizMollica <biamollica@gmail.com>
2024-10-08 15:41:39 -0300

    terminei o projeto

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let nome = "Beatriz"
-console.log(nome) 
+let nomedoHeroi = "Rafael"
+let totalExperiencia = 6798
+
+if(totalExperiencia <= 1000){
+	totalExperiencia = "Ferro"
+} else if (totalExperiencia <= 2000){
+	totalExperiencia = "bronze"
+} else if (totalExperiencia <= 5000){
+	totalExperiencia = "Prata"
+} else if (totalExperiencia <= 7000){
+	totalExperiencia = "Ouro"
+} else if (totalExperiencia <= 8000){
+	totalExperiencia = "Platina"
+} else if (totalExperiencia <= 9000){
+	totalExperiencia = "Ascendente"
+} else if (totalExperiencia <= 10000){
+	totalExperiencia = "Imortal"
+} else{
+	totalExperiencia = "Radiante"
+}
+
+console.log("O Herói de nome " + nomedoHeroi, "e está no nível de " + totalExperiencia)
